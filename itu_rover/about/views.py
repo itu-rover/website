da5d14172f535018df58790d3bbf70b20a2216d9
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2018-01-24 19:06:58 +0300

    Write about page view and draft related template

--- itu_rover/about/views.py
+++ itu_rover/about/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import AboutEntry
+
+
+class AboutPage(ListView):
+    model = AboutEntry
+    template_name = 'about.html'
+    context_object_name = 'entries'
--- itu_rover/itu_rover/urls.py
+++ itu_rover/itu_rover/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from about.views import AboutPage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hakkinda/', AboutPage.as_view(), name='about'),
 ]
