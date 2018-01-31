217bfaeea9ac3ff5d99b0c6aa59871fc4ca82e79
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2018-01-31 16:51:05 +0300

    Configure static and media serving for development

--- itu_rover/core/utils.py
+++ itu_rover/core/utils.py
@@ -2,8 +2,11 @@ from members.models import TeamLeader
 
 
 def leader_phone_and_email(request):
-    leader_obj = TeamLeader.objects.select_related('leader').get()
-    return {
-        'phone': leader_obj.leader.phone,
-        'email': leader_obj.leader.email,
-    }
+    try:
+        leader_obj = TeamLeader.objects.select_related('leader').get()
+        return {
+            'phone': leader_obj.leader.phone,
+            'email': leader_obj.leader.email,
+        }
+    except AttributeError:
+        return {'phone': '', 'email': 'ituroverteam@gmail.com'}
--- itu_rover/itu_rover/urls.py
+++ itu_rover/itu_rover/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.conf import settings
 
 from about.views import AboutPage
@@ -29,6 +30,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
+    urlpatterns = ([
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+      + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+      + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
