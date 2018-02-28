7f4c6bfd3a652aff22b11cfcc0aa6f8d22de2600
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2018-02-28 18:32:33 +0300

    Register document model in admin

--- itu_rover/core/admin.py
+++ itu_rover/core/admin.py
@@ -5,8 +5,11 @@ from django.contrib.auth.models import User, Group
 from adminsortable2.admin import SortableAdminMixin
 from pagedown.widgets import AdminPagedownWidget
 
+from .models import Document
+
 
 admin.site.unregister([User, Group])
+admin.site.register(Document)
 
 
 class PagedownedModelAdmin(admin.ModelAdmin):
