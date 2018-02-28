6073bc487e08746f07659b118f3e13fede522a0c
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2018-02-28 16:38:08 +0300

    Unregister user and group models from admin

--- itu_rover/core/admin.py
+++ itu_rover/core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 from adminsortable2.admin import SortableAdminMixin
 from pagedown.widgets import AdminPagedownWidget
+
+
+admin.site.unregister([User, Group])
 
 
 class PagedownedModelAdmin(admin.ModelAdmin):
