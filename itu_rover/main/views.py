35a2f5a746d0c9978d6a008625dc0490a3b6e036
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2018-02-28 17:23:56 +0300

    Add main page news to main app template

--- itu_rover/main/views.py
+++ itu_rover/main/views.py
@@ -1,9 +1,16 @@
-from django.views.generic.list import ListView
+from django.views.generic import TemplateView
 
-from .models import SliderImage
+from .models import SliderImage, MainPageEntry
 
 
-class MainPage(ListView):
-    model = SliderImage
+class MainPage(TemplateView):
     template_name = 'main.html'
-    context_object_name = 'slide_images'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        extra_context = {
+            'slide_images': SliderImage.objects.all(),
+            'entries': MainPageEntry.objects.filter(is_old=False),
+        }
+        context.update(extra_context)
+        return context
