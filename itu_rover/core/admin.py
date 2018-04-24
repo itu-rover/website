442c51b985df1181aa8ea3c13bb1a9124179590b
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2018-04-24 14:21:12 +0300

    Add core image model

--- itu_rover/core/admin.py
+++ itu_rover/core/admin.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User, Group
 from adminsortable2.admin import SortableAdminMixin
 from pagedown.widgets import AdminPagedownWidget
 
-from .models import Document
+from .models import Document, Image
 
 
 admin.site.unregister([User, Group])
-admin.site.register(Document)
+admin.site.register([Document, Image])
 
 
 class PagedownedModelAdmin(admin.ModelAdmin):
--- itu_rover/core/models.py
+++ itu_rover/core/models.py
@@ -24,3 +24,13 @@ class Document(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=20)
+    image = models.ImageField(
+        upload_to='images/other',
+    )
+
+    def __str__(self):
+        return self.title
