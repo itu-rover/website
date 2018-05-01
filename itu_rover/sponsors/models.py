e28a4ef0cb6b490e932838560657dd8e870f9073
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2018-05-01 16:31:20 +0300

    Add ordering for sponsors

--- itu_rover/sponsors/models.py
+++ itu_rover/sponsors/models.py
@@ -32,6 +32,9 @@ class Sponsor(TimeStampedModel):
         verbose_name='is sponsor old?',
     )
 
+    class Meta:
+        ordering = ('name', )
+
     def __str__(self):
         return self.name
 
