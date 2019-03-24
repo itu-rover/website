3db804537bc756c267697554fe0ea685f4a0a030
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2019-03-24 22:52:18 +0300

    Fix multiple available team leaders for navbar problem

--- itu_rover/core/utils.py
+++ itu_rover/core/utils.py
@@ -3,11 +3,14 @@ from django.core.exceptions import ObjectDoesNotExist
 from oldyears.models import OldYear
 from members.models import TeamLeader
 
+from .defaults import current_year
+
 
 def attach_for_navbar(request):
     rover_mail = 'ituroverteam@gmail.com'
     try:
         leader_obj = (TeamLeader.objects
+                      .filter(member__year=current_year())
                       .select_related('member').get())
         phone = leader_obj.member.phone
     except AttributeError:
